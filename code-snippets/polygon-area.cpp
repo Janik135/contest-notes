b2ae95ca7f82f683356b70db57dc268c8e447e16
Janik135 <Janik135@users.noreply.github.com>
2018-11-26 12:27:43 +0100

    Correcting polygon-area

--- code-snippets/polygon-area.cpp
+++ code-snippets/polygon-area.cpp
@@ -3,7 +3,8 @@ vector<struct point> points;
 double area = 0;
 for (long i = 0; i < points.size(); i++) {
     long j = (i + 1) % n;
-    area += p[i].x * p[j].y;
-    area -= p[i].y * p[j].x;
+    area += points[I].x * points[j].y;
+    area -= points[I].y * points[j].x;
 }
+area /= 2;
 // ...
